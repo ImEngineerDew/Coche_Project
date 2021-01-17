f6cbcd65db225ff97cf51f4e8e189f81161664a2
Dewin Acosta <dewin.acosta9207@gmail.com>
2021-01-17 18:32:58 -0500

    Ya vuelvo a revisar el branch abstractCoche

--- Coche/Principal/Main_Camioneta.java
+++ Coche/Principal/Main_Camioneta.java
@@ -1,6 +1,11 @@
 package Coche_Proyecto.Coche.Principal;
+import Coche_Proyecto.Coche.Coche;
 
 public class Main_Camioneta extends Coche
 {
+    @Override
+    public void getInfo()
+    {
 
+    }
 }
