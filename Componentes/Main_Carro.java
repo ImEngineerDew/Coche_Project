88cd87a25c7150b565d9812c6ce9b68b7e0a2aa0
Dewin Acosta <dewin.acosta9207@gmail.com>
2021-01-18 16:25:46 -0500

    Now, I've added a conditionals about of that the car is public or private service

--- Componentes/Main_Carro.java
+++ Componentes/Main_Carro.java
@@ -36,21 +36,44 @@ public class Main_Carro extends Coche
         System.out.println("Ingrese la marca del coche: ");          String marca   = objeto.nextLine();
         System.out.println("Ingrese el tipo de motor: ");            String motor   = objeto.nextLine();
         System.out.println("¿El coche tiene asientos de cuero?: ");  String entrada = objeto.nextLine();
+        System.out.println("¿Servicio público?: ");                  String publico = objeto.nextLine();
 
         renault.setMarca(marca);
         renault.setTipo_motor(motor);
 
         if(renault.asientosDeCuero(entrada)== true)
         {
-            System.out.println("Marca: "+renault.getMarca());
-            System.out.println("Tipo de motor: "+renault.getTipo_motor());
-            System.out.println("El coche tiene asientos de cuero");
+            if(renault.servicioPublico(publico) == true)
+            {
+                System.out.println("Marca: " + renault.getMarca());
+                System.out.println("Tipo de motor: " + renault.getTipo_motor());
+                System.out.println("El coche tiene asientos de cuero");
+                System.out.println("El coche es de servicio público");
+            }
+            else
+            {
+                System.out.println("Marca: " + renault.getMarca());
+                System.out.println("Tipo de motor: " + renault.getTipo_motor());
+                System.out.println("El coche tiene asientos de cuero");
+                System.out.println("El coche es partícular");
+            }
         }
         else if(renault.asientosDeCuero(entrada)== false)
         {
-            System.out.println("Marca: "+renault.getMarca());
-            System.out.println("Tipo de motor: "+renault.getTipo_motor());
-            System.out.println("El coche no tiene asientos de cuero");
+            if(renault.servicioPublico(publico) == true)
+            {
+                System.out.println("Marca: " + renault.getMarca());
+                System.out.println("Tipo de motor: " + renault.getTipo_motor());
+                System.out.println("El coche no tiene asientos de cuero");
+                System.out.println("El coche es de servicio público");
+            }
+            else
+            {
+                System.out.println("Marca: " + renault.getMarca());
+                System.out.println("Tipo de motor: " + renault.getTipo_motor());
+                System.out.println("El coche no tiene asientos de cuero");
+                System.out.println("El coche es partícular");
+            }
         }
     }
 
