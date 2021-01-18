4a6f7e0d19a14971b8b528aba00a49041b62309a
Dewin Acosta <dewin.acosta9207@gmail.com>
2021-01-18 16:38:10 -0500

    Testing with arrayList for the adding many kind of cars

---
+++ MenuUI/arrayTesting.java
@@ -0,0 +1,10 @@
+package Coche_Proyecto.MenuUI;
+import java.util.ArrayList;
+
+public class arrayTesting
+{
+    public static void main(String args[])
+    {
+        ArrayList <arreglo> = new ArrayList<>();
+    }
+}
