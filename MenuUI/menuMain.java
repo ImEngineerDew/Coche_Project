b935700332f2636649dfdf67956426da4844ca4c
Dewin Acosta <dewin.acosta9207@gmail.com>
2021-01-17 19:49:43 -0500

    Now I am working on the main menu

--- MenuUI/menuMain.java
+++ MenuUI/menuMain.java
@@ -1,6 +1,14 @@
 package Coche_Proyecto.MenuUI;
+import java.util.Scanner;
 
 public class menuMain
 {
+    public static void main (String args[])
+    {
+        Scanner lista = new Scanner(System.in);
 
+        do {
+
+        }while();
+    }
 }
