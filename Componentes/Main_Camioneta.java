a0c7706aa3381d06dad9d59b2e25823f1435645d
Dewin Acosta <dewin.acosta9207@gmail.com>
2021-01-27 17:30:04 -0500

    Only prints garbage, I need to print the index trucks

--- Componentes/Main_Camioneta.java
+++ Componentes/Main_Camioneta.java
@@ -10,6 +10,11 @@ public class Main_Camioneta extends Coche
 
    ArrayList <Coche> cocheModelo = new ArrayList<>();
 
+   public void addCamioneta (Coche agregado)
+   {
+       cocheModelo.add(agregado);
+   }
+
    public Main_Camioneta()
    {
       super();
@@ -46,11 +51,28 @@ public class Main_Camioneta extends Coche
        return pasajeros;
    }
 
+   @Override
+   public void getInfo()
+   {
+
+   }
+
    public void mostrarCamioneta()
    {
        Scanner objeto = new Scanner(System.in);
-       Main_Camioneta foton = new Main_Camioneta();
+       Main_Camioneta foton = new Main_Camioneta(4, 4, "Gasolina", "Foton", "si", 4000, "Frutas");
+       Main_Camioneta renault = new Main_Camioneta(4, 4, "Gas natural", "Renault", "no", 4000, "Ganado");
 
+       cocheModelo.add(foton);
+       cocheModelo.add(renault);
+
+       for (int i=0; i<cocheModelo.size(); i++)
+       {
+           Coche listado = cocheModelo.get(i);
+           System.out.println(listado);
+       }
+
+       /*
        System.out.println("La camioneta tiene asientos de cuero: "); String asientos = objeto.nextLine();
        System.out.println("Ingrese la marca de la camioneta: ");     String marca_camioneta = objeto.nextLine();
        System.out.println("Ingrese el tipo de motor: ");             String combustible     = objeto.nextLine();
@@ -58,14 +80,14 @@ public class Main_Camioneta extends Coche
        System.out.println("Ingrese el numero de acompañantes: ");    int numero_acompañantes = objeto.nextInt();
        System.out.println("Ingrese la capacidad de carga: ");        int capacidad_carga = objeto.nextInt();
 
-       /*Espacio para almacenar los datos en los setters*/
+       //Espacio para almacenar los datos en los setters
        foton.setMarca(marca_camioneta);
        foton.setTipo_motor(combustible);
        foton.setCapacidad(capacidad_carga);
        foton.setTipo_carga(load);
        foton.numeroPasajeros(numero_acompañantes);
 
-       /*Comparativa para imprimir datos*/
+       //Comparativa para imprimir datos
        if(foton.asientosDeCuero(asientos)== true)
        {
           System.out.println("Marca camioneta: "+foton.getMarca());
@@ -85,10 +107,7 @@ public class Main_Camioneta extends Coche
           System.out.println("La camioneta no tiene asientos de cuero");
          }
        }
+       */
 
-    @Override
-    public void getInfo()
-    {
-        System.out.println("This is just a test from the implementation about of abstract class");
-    }
+   }
 }
