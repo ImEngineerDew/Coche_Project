3c6b8b65fef1830e63f7794f87500445301f1efc
Dewin Acosta <dewin.acosta9207@gmail.com>
2021-01-27 16:57:28 -0500

    I'm back

--- MenuUI/arrayTesting.java
+++ MenuUI/arrayTesting.java
@@ -1,15 +1,9 @@
 package Coche_Proyecto.MenuUI;
-import Coche_Proyecto.Componentes.Main_Camioneta;
-import Coche_Proyecto.Componentes.Main_Carro;
-
-import java.util.ArrayList;
-
 
 public class arrayTesting
 {
     public static void main(String args[])
     {
-        ArrayList <Main_Carro> lista = new ArrayList<>();
 
     }
 }
