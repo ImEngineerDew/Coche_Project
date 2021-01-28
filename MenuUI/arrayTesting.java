d1f2f0bb46ad53542c46508b594fe2273a5cd059
Dewin Acosta <dewin.acosta9207@gmail.com>
2021-01-27 22:28:28 -0500

    Testing a ArrayList sample

--- MenuUI/arrayTesting.java
+++ MenuUI/arrayTesting.java
@@ -1,9 +1,34 @@
 package Coche_Proyecto.MenuUI;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class arrayTesting
 {
     public static void main(String args[])
     {
+        ArrayList <String> cadenas = new ArrayList<>();
+        Scanner objeto  = new Scanner(System.in);
+
+        boolean isRunnable = true;
+        System.out.println("Running...");
+
+        while(isRunnable)
+        {
+            System.out.println("Please write your name: ");
+            String name = objeto.nextLine();
+
+            if(name.equalsIgnoreCase("exit"))
+            {
+                break;
+            }
+            cadenas.add(name);
+        }
+
+        for(String recorrido: cadenas)
+        {
+            System.out.println(cadenas.toString());
+        }
+
 
     }
 }
