47acb56bd1113a09a80e139283f2ceff67ec8128
Dewin Acosta <dewin.acosta9207@gmail.com>
2021-01-28 13:51:47 -0500

    This is the first commit using the arrayList testing for adding more data

--- MenuUI/arrayTesting.java
+++ MenuUI/arrayTesting.java
@@ -4,6 +4,47 @@ import java.util.Scanner;
 
 public class arrayTesting
 {
+    private String nameData;
+    private String surnameData;
+    private int ageData;
+
+    public arrayTesting ()
+    {
+        nameData= "John";
+        surnameData = "Doe";
+        ageData = 25;
+    }
+
+    public arrayTesting(String nameData,String surnameData,int ageData)
+    {
+        this.nameData = nameData;
+        this.surnameData = surnameData;
+        this.ageData = ageData;
+    }
+
+    public String getNameData()
+    {
+        return nameData;
+    }
+    public String getSurnameData()
+    {
+        return surnameData;
+    }
+    public int getAgeData()
+    {
+        return ageData;
+    }
+
+    public void setNameData(String nameData)
+    {
+        this.nameData = nameData;
+    }
+    public void setSurnameData(String surnameData)
+    {
+        this.surnameData = surnameData;
+    }
+
+
     public static void main(String args[])
     {
         ArrayList <String> cadenas = new ArrayList<>();
