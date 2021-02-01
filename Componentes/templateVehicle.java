5b4a7eacb9533d8f8dbc81cffaa0290da6cda629
Dewin Acosta <dewin.acosta9207@gmail.com>
2021-01-31 21:43:43 -0500

    Writting the inputs of the templateVehicle.java

--- Componentes/templateVehicle.java
+++ Componentes/templateVehicle.java
@@ -1,11 +1,12 @@
 package Coche_Proyecto.Componentes;
+
+
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class templateVehicle extends Coche {
-    private String esPublico;
-    private int passengersQuantity;
 
+    private String esPublico;
     private ArrayList <templateVehicle> carModel = new ArrayList <>();
 
     private void addCar(templateVehicle agregado)
@@ -47,7 +48,13 @@ public class templateVehicle extends Coche {
     }
 
     public void mostrarCarro() {
-        
+
+        Scanner object = new Scanner(System.in);
+
+        System.out.println("Please write the brand of the car: ");          String brand = object.next();
+        System.out.println("Which kind of the fuel has the car?: ");        String fuel = object.next();
+        System.out.println("Has the truck the leather seats? :");           String leather = object.next();
+
+        templateVehicle car = new templateVehicle();
     }
 }
-
