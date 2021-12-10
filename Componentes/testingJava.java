9275ddfc65ddd57fc79b846a66117d7212451fe0
Dewin Acosta <dewin.acosta9207@gmail.com>
2021-12-09 21:39:45 -0500

    Let's introduce the emptiness folder between front and backend

--- Componentes/testingJava.java
+++ Componentes/testingJava.java
@@ -2,16 +2,15 @@ import java.util.Scanner;
 
 public class testingJava
 {
-
 	public static void main (String args[])
 	{
-		
 		Scanner input = new Scanner(System.in);
 		
 		System.out.println("Write your text: ");
 		String  stringInput =  input.nextLine();
 
-		System.out.println("Your text typed is: "+stringInput);
+
+		System.out.println("Your text 	typed is: "+stringInput);
 	}
 }
 
