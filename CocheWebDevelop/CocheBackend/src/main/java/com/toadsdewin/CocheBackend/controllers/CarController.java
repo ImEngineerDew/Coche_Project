60acc696e4e68238aa7d948a6e0d70618de1df13
Dewin Acosta <dewin.acosta9207@gmail.com>
2021-12-09 23:29:34 -0500

    Recent changes

--- CocheWebDevelop/CocheBackend/src/main/java/com/toadsdewin/CocheBackend/controllers/CarController.java
+++ CocheWebDevelop/CocheBackend/src/main/java/com/toadsdewin/CocheBackend/controllers/CarController.java
@@ -1,4 +1,64 @@
 package com.toadsdewin.CocheBackend.controllers;
 
-public class CarController {
+import com.toadsdewin.CocheBackend.models.CarModel;
+import com.toadsdewin.CocheBackend.models.UserModel;
+import com.toadsdewin.CocheBackend.services.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class CarController
+{
+    @Autowired
+    CarService carService;
+
+    @PostMapping("/cars")
+    public ResponseEntity<Map<String,String>> saveCars(@Valid @RequestBody CarModel car)
+    {
+        this.carService.save(car);
+        Map<String,String> answer  = new HashMap<>();
+        answer.put("Message","The car has been introduced properly");
+        answer.put("Status","true");
+
+        return ResponseEntity.ok(answer);
+    }
+
+    @PutMapping("/cars")
+    public ResponseEntity<Map<String,String>> upgradeCars(@Valid @RequestBody CarModel car)
+    {
+        this.carService.save(car);
+        Map<String,String> answer  = new HashMap<>();
+        answer.put("Message","The car has been updated properly");
+        answer.put("Status","true");
+
+        return ResponseEntity.ok(answer);
+    }
+
+    @GetMapping("/cars")
+    public List<CarModel> getUsers()
+    {
+        return this.carService.getCar();
+    }
+
+    /**This is the linecode that find the users by id**/
+    @GetMapping("/cars/{id}")
+    public CarModel getCarById(@PathVariable String id)
+    {
+        return this.carService.findById(id).get();
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/cars/{id}")
+    public String deleteCar(@PathVariable String id)
+    {
+        carService.deleteUserbyId(id);
+        return "Car deleted by id";
+    }
+
 }
---
+++ CocheWebDevelop/CocheBackend/src/main/java/com/toadsdewin/CocheBackend/services/CarService.java
@@ -0,0 +1,34 @@
+package com.toadsdewin.CocheBackend.services;
+import com.toadsdewin.CocheBackend.models.CarModel;
+import com.toadsdewin.CocheBackend.models.UserModel;
+import com.toadsdewin.CocheBackend.repositories.CarRepository;
+import com.toadsdewin.CocheBackend.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CarService {
+    @Autowired
+    CarRepository carRepository;
+
+    //A method for save the user data;
+    public void save(CarModel car) {
+        this.carRepository.save(car);
+    }
+
+    public List<CarModel> getCar() {
+        return this.carRepository.findAll();
+    }
+
+    public Optional<CarModel> findById(String id) {
+        return this.carRepository.findById(id);
+    }
+
+    public void deleteUserbyId(String id)
+    {
+        carRepository.deleteById(id);
+    }
+}
